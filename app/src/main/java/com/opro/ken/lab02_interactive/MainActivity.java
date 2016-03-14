bf2b449aea6f872919e846f0f6a148ab9e37add6
KenTong <motestt59642@gmail.com>
2016-03-14 10:36:31 +0800

    +- button

--- app/src/main/java/com/opro/ken/lab02_interactive/MainActivity.java
+++ app/src/main/java/com/opro/ken/lab02_interactive/MainActivity.java
@@ -14,24 +14,34 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void submitOrder(View view) {
-        display(i*50);
+        display(i * 50);
     }
 
     private void display(int num2) {
         TextView tv2 = (TextView) findViewById(R.id.tv2);
         tv2.setText(String.valueOf(num2));
+        if (num2 > 0) {
+            tv2.setText(String.valueOf(num2 + "Thank you:)"));
+        }
+        if (num2 == 0) {
+            tv2.setText(String.valueOf(num2 + "Free!!!"));
+        }
     }
     int i = 0;
 
     public void a(View view) {
         aa(++i);
     }
+
     public void b(View view) {
-        aa(--i);
+        if (i > 0) {
+            i--;
+            aa(i);
+        }
     }
+
     private void aa(int num1) {
         TextView tv1 = (TextView) findViewById(R.id.tv1);
         tv1.setText(String.valueOf(num1));
     }
-
 }
