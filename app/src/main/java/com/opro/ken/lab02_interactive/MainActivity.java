64129f90525361abbe4a6a5ae28dd5ed5cf767c5
KenTong <motestt59642@gmail.com>
2016-03-14 11:13:34 +0800

    +- button

--- app/src/main/java/com/opro/ken/lab02_interactive/MainActivity.java
+++ app/src/main/java/com/opro/ken/lab02_interactive/MainActivity.java
@@ -5,7 +5,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
 
+import java.text.NumberFormat;
+
 public class MainActivity extends AppCompatActivity {
+
+    int i = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,20 +18,25 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void submitOrder(View view) {
-        display(i * 50);
+        display(i);
     }
 
     private void display(int num2) {
         TextView tv2 = (TextView) findViewById(R.id.tv2);
-        tv2.setText(String.valueOf(num2));
+        int total = 50 * num2;
+        String myString = NumberFormat.getCurrencyInstance().format(total);
+        tv2.setText(myString);
+
+
         if (num2 > 0) {
-            tv2.setText(String.valueOf(num2 + "Thank you:)"));
+            String myString1 = NumberFormat.getCurrencyInstance().format(total);
+            tv2.setText(myString1+ "\nThank you:)");
         }
-        if (num2 == 0) {
-            tv2.setText(String.valueOf(num2 + "Free!!!"));
+        else if (num2 == 0) {
+            String myString1 = NumberFormat.getCurrencyInstance().format(total);
+            tv2.setText(myString1+ "\nFree!!!");
         }
     }
-    int i = 0;
 
     public void a(View view) {
         aa(++i);
