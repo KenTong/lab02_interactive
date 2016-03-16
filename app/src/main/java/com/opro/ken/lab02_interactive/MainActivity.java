13441dc0b8bde715fb8b5ed034c97f4403290377
KenTong <motestt59642@gmail.com>
2016-03-16 12:31:04 +0800

    3/16 New

--- app/src/main/java/com/opro/ken/lab02_interactive/MainActivity.java
+++ app/src/main/java/com/opro/ken/lab02_interactive/MainActivity.java
@@ -3,14 +3,16 @@ package com.opro.ken.lab02_interactive;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.CheckBox;
 import android.widget.TextView;
-
 import java.text.NumberFormat;
 
 public class MainActivity extends AppCompatActivity {
 
     int mQuantity = 0;
     int mPrice = 50;
+    String name = "Ken";
+    boolean ck;
 
 
     @Override
@@ -20,42 +22,55 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void submitOrder(View view) {
-        display();
+        displayTotalPrice();
     }
 
-    private void display() {
+    private void displayTotalPrice() {
         TextView tv2 = (TextView) findViewById(R.id.tv2);
-        int total = mPrice * mQuantity ;
+        int total = mPrice * mQuantity;
         String myString = NumberFormat.getCurrencyInstance().format(total);
-        tv2.setText(myString);
-
-
-        if (mQuantity > 0) {
-            String myString1 = NumberFormat.getCurrencyInstance().format(total);
-            tv2.setText(myString1+ "\nThank you:)");
-
-        }
-        else if (mQuantity == 0) {
-            String myString1 = NumberFormat.getCurrencyInstance().format(total);
-            tv2.setText(myString1+ "\nFree!!!");
-        }
+        String message ="Name: "+name+"\n臭豆腐"+"\n加泡菜? "+ ck+"\nQuantity "+mQuantity+" \n"
+                + myString+ (mQuantity == 0 ? "\nFree!!!" : "\nThank you:)");
+        tv2.setText(message);
     }
 
     public void incement(View view) {
         ++mQuantity;
-        aa();
+        displayQuantity();
+
 
     }
 
     public void decement(View view) {
         if (mQuantity > 0) {
-            mQuantity--;
-            aa();
+            --mQuantity;
+            displayQuantity();
+
+        }
+    }
+    public void Check(){
+        final CheckBox toppings = (CheckBox) findViewById(R.id.toppings);
+        if (toppings.isChecked())
+        {
+            toppings.setChecked(false);
+            ck=(true);
         }
     }
 
-    private void aa() {
+
+    //private void resetTotalPrice() {
+        //TextView tv1 = (TextView) findViewById(R.id.tv1);
+        //tv1.setText("");
+    //}
+
+    private void displayQuantity() {
         TextView tv1 = (TextView) findViewById(R.id.tv1);
         tv1.setText(String.valueOf(mQuantity));
+
+    }
+    public void add(View view) {
+        final CheckBox toppings = (CheckBox) findViewById(R.id.toppings);
+        toppings.setChecked(true);
+        ck=(true);
     }
 }
