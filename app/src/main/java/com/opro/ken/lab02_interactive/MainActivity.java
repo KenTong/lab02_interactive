0fd678fc86146be5d024c7b37c549a6d559ae1b0
KenTong <motestt59642@gmail.com>
2016-03-17 12:26:53 +0800

    3/16 New

--- app/src/main/java/com/opro/ken/lab02_interactive/MainActivity.java
+++ app/src/main/java/com/opro/ken/lab02_interactive/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.CheckBox;
 import android.widget.TextView;
+
 import java.text.NumberFormat;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,13 +13,27 @@ public class MainActivity extends AppCompatActivity {
     int mQuantity = 0;
     int mPrice = 50;
     String name = "Ken";
-    boolean ck;
+    StringBuilder PriceMessage = new StringBuilder("NT$ " + mPrice);
+    StringBuilder QuantityMessage = new StringBuilder(mQuantity);
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        displayMessage();
+    }
+    private void displayMessage() {
+        TextView tv2 = (TextView) findViewById(R.id.tv2);
+        tv2.setText(PriceMessage);
+    }
+
+    private void displayQuantity() {
+        TextView tv1 = (TextView) findViewById(R.id.tv1);
+        int start = 0;
+        int end = QuantityMessage.length();
+        QuantityMessage.delete(start, end).append(mQuantity);
+        tv1.setText(QuantityMessage);
     }
 
     public void submitOrder(View view) {
@@ -27,46 +42,50 @@ public class MainActivity extends AppCompatActivity {
 
     private void displayTotalPrice() {
         TextView tv2 = (TextView) findViewById(R.id.tv2);
+        CheckBox toppings = (CheckBox) findViewById(R.id.toppings);
         int total = mPrice * mQuantity;
         String myString = NumberFormat.getCurrencyInstance().format(total);
-        String message ="Name: "+name+"\n臭豆腐"+"\n加泡菜? "+ ck+"\nQuantity "+mQuantity+" \n"
-                + myString+ (mQuantity == 0 ? "\nFree!!!" : "\nThank you:)");
+        String message = "Name: " + name +
+                "\n臭豆腐" + "\n加泡菜? " +
+                toppings.isChecked() +
+                "\nQuantity " + mQuantity + " \n"
+                + myString + (mQuantity == 0 ?
+                "\nFree!!!" : "\nThank you:)");
         tv2.setText(message);
     }
 
     public void incement(View view) {
         ++mQuantity;
+        resetPriceMessage();
+        displayMessage();
         displayQuantity();
-
-
     }
-
     public void decement(View view) {
         if (mQuantity > 0) {
             --mQuantity;
+            resetPriceMessage();
+            displayMessage();
             displayQuantity();
 
         }
     }
 
-    //private void resetTotalPrice() {
-        //TextView tv1 = (TextView) findViewById(R.id.tv1);
-        //tv1.setText("");
-    //}
+       private void clearPriceMessage() {
+            int start = 0;
+            int end = PriceMessage.length();
+            PriceMessage.delete(start, end);
+        }
 
-    private void displayQuantity() {
-        TextView tv1 = (TextView) findViewById(R.id.tv1);
-        tv1.setText(String.valueOf(mQuantity));
-
-    }
     public void add(View view) {
-        final CheckBox toppings = (CheckBox) findViewById(R.id.toppings);
-        if (toppings.isChecked())
-    {
-        toppings.setChecked(false);
-        ck=(false);
-        toppings.setChecked(true);
-        ck=(true);
+        resetPriceMessage();
+        displayMessage();
+        }
+
+    private void resetPriceMessage() {
+        clearPriceMessage();
+        PriceMessage.append("臭豆腐").append("NT$ ").append(mPrice);
+
     }
-    }
+
+
 }
